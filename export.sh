7eda152121ba4506347056ec098abf4d909a8026
Gaëtan Renaudeau <gre@zenexity.com>
2010-12-05 16:15:52 +0100

    bugfix export.sh

--- export.sh
+++ export.sh
@@ -1,9 +1,9 @@
 tarname="morpion-gaetan.renaudeau.tar.gz"
 directory="morpion-gaetan.renaudeau"
-rm -f $directory
+rm -rf $directory
 mkdir $directory
 cp *.c $directory
 cp *.h $directory
 cp Makefile $directory
 tar cvzf $tarname $directory
-rm -f $directory
+rm -rf $directory
